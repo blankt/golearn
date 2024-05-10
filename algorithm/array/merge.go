519200719bbd587faa874701950d5680389f0b9c
tlf <534464210@qq.com>
2024-05-10 11:17:33 +0800

    56.合并区间

--- algorithm/array/merge.go
+++ algorithm/array/merge.go
@@ -1,10 +1,43 @@
 package main
 
-func merge(intervals [][]int) [][]int {
+import (
+	"fmt"
+	"sort"
+)
 
-	return nil
+func merge(intervals [][]int) [][]int {
+	sort.Slice(intervals, func(i, j int) bool {
+		if intervals[i][0] < intervals[j][0] {
+			return true
+		} else {
+			return false
+		}
+	})
+
+	result := make([][]int, len(intervals))
+	for k := range result {
+		result[k] = make([]int, 2)
+	}
+	index := -1
+	for i := 0; i < len(intervals); i++ {
+		if index == -1 || intervals[i][0] > result[index][1] {
+			index++
+			result[index] = intervals[i]
+		} else {
+			if result[index][1] < intervals[i][1] {
+				result[index][1] = intervals[i][1]
+			}
+		}
+	}
+
+	if index == len(result)-1 {
+		return result
+	} else {
+		return result[:index+1]
+	}
 }
 
 func main() {
-
+	intervals := [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}
+	fmt.Println(merge(intervals))
 }
